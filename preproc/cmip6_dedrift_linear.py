2f6a0a15ceed25c9c38090cc3a902bd22ff290d3
Tim Hermans <timthjhermans@gmail.com>
2020-10-06 14:41:06 +0200

    Update cmip6_dedrift_linear.py

--- preproc/cmip6_dedrift_linear.py
+++ preproc/cmip6_dedrift_linear.py
@@ -161,9 +161,10 @@ def cmip6_dedrift_linear(in_dir,out_dir,variable):
     
 ### for testing:
 '''
-variable = 'zostoga'
-in_dir = '/Volumes/Naamloos/PhD_Data/CMIP6/downloading/07_2020/time_merged/' + variable #set raw, time-merged zostoga directory
-out_dir = '/Volumes/Naamloos/PhD_Data/CMIP6/downloading/07_2020/linear_dedrifted/' + variable #set output directory
+if __name__ == '__main__':
+    variable = 'zostoga'
+    in_dir = '/Volumes/Naamloos/PhD_Data/CMIP6/downloading/07_2020/time_merged/' + variable #set raw, time-merged zostoga directory
+    out_dir = '/Volumes/Naamloos/PhD_Data/CMIP6/downloading/07_2020/linear_dedrifted/' + variable #set output directory
 
-cmip6_dedrift_linear(in_dir,out_dir,variable)
+    cmip6_dedrift_linear(in_dir,out_dir,variable)
 '''
